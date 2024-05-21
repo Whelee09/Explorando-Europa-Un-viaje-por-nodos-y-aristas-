8e7ca7975090c56d3c528cfef8933c16234fee47
name <williangel10@outlook.com>
2024-05-21 16:20:22 -0500

    agregando nuevos archivos al proyecto

---
+++ rsc/Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public Data (){ 
+        //readFileData()    
+    }
+    public void getDataFlightToEurope(){
+        System.out.println("HI");
+    }
+    public void getDataTrainInEurope(){
+        System.out.println("HI");
+    }
+    public void getDataFlightInEurope(){
+        System.out.println("HI");
+    }
+
+}
---
+++ src/Tour.java
@@ -0,0 +1,33 @@
+//import rsc.Data;
+
+public class Tour {
+    Tour(){
+       //TODO 
+       // getData();
+       //destokenizarData();
+    }
+
+
+
+    public void selectAPlan(){
+        System.out.println("Ingrese la fecha de partida");
+        System.out.println("Ingrese la fecha de regreso");
+
+        System.out.println("marque los paises a visitar");
+
+        System.out.println("Elija un plan: ");
+        System.out.println("Visitar la mayor cantidad de landmarks lo mas economico posible");
+        System.out.println("Visitar la mayor cantidad de landmarks lo mas rapido posible");
+        System.out.println("con comodidad pero si es necesario perder algunas de landmarks");
+    }
+
+    public void calcularViaje(){
+        //calcEconomicAsPosible();
+        //calcFastestAsPossible();
+        //calcConfortableAsPossible();
+    }
+
+    // Data data = new Data();
+
+
+}
